114a7b7575a3c7046c2651bf240b60a7f8cffaf5
wulman16 <will.ulman@gmail.com>
2019-06-04 11:43:38 -0400

    add Doubly Linked List class

---
+++ DoublyLinkedList.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.prev = null;
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
