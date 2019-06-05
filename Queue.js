9d547627d9bbd018f536ccbb946e60314e40e56d
wulman16 <will.ulman@gmail.com>
2019-06-05 12:09:21 -0400

    add enqueue and dequeue methods

--- Queue.js
+++ Queue.js
@@ -11,4 +11,20 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(val) {
+    const newLast = new Node(val);
+    this.size === 0 ? (this.first = newLast) : (this.last.next = newLast);
+    this.last = newLast;
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (this.size === 0) return;
+    const dequeued = this.first;
+    if (this.size === 1) this.last = null;
+    this.first = dequeued.next;
+    this.size--;
+    return dequeued;
+  }
 }
