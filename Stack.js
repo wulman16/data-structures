21a82da1f4680a3c63a8d796dbefa6a28b29c349
wulman16 <will.ulman@gmail.com>
2019-06-05 11:50:18 -0400

    debug Stack push and pop methods

--- Stack.js
+++ Stack.js
@@ -14,26 +14,19 @@ class Stack {
 
   push(val) {
     const newFirst = new Node(val);
-    this.size === 0
-      ? (this.last = newFirst)
-      : (newFirst.next = this.first.next);
+    this.size === 0 ? (this.last = newFirst) : (newFirst.next = this.first);
     this.first = newFirst;
     return ++this.size;
   }
 
   pop() {
-    // if length is 0 return undefined
-    // grab old head
-    // set new head to be old head's next
-    // set old head's next to null
-    // return decremented length
     if (this.size === 0) return;
+    const popped = this.first;
     if (this.size === 1) {
-      this.first = null;
       this.last = null;
-    } else {
-      this.head = this.head.next;
     }
-    return --this.size;
+    this.first = this.first.next;
+    this.size--;
+    return popped.val;
   }
 }
