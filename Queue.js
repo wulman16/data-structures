b45699ec13a7f092822a426d227554dca75e73bc
wulman16 <will.ulman@gmail.com>
2019-06-05 11:57:09 -0400

    initialize Queue class

---
+++ Queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
