b24b9c6b16310d129f62d753b264ee2be05b4f38
wulman16 <will.ulman@gmail.com>
2019-06-05 11:41:23 -0400

    implement push and pop methods for stack class

--- Stack.js
+++ Stack.js
@@ -11,4 +11,29 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+
+  push(val) {
+    const newFirst = new Node(val);
+    this.size === 0
+      ? (this.last = newFirst)
+      : (newFirst.next = this.first.next);
+    this.first = newFirst;
+    return ++this.size;
+  }
+
+  pop() {
+    // if length is 0 return undefined
+    // grab old head
+    // set new head to be old head's next
+    // set old head's next to null
+    // return decremented length
+    if (this.size === 0) return;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.head = this.head.next;
+    }
+    return --this.size;
+  }
 }
