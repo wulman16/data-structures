66ff189f6f683432694706b2a0cc7135f87e5efc
wulman16 <will.ulman@gmail.com>
2019-06-06 16:00:43 -0400

    implement enqueue method for priority queue

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -9,4 +9,21 @@ class PriorityQueue {
   constructor() {
     this.values = [];
   }
+
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    let idx = this.values.length - 1;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    while (idx > 0) {
+      if (this.values[parentIdx].priority <= newNode.priority) break;
+      [this.values[parentIdx], this.values[idx]] = [
+        this.values[idx],
+        this.values[parentIdx]
+      ];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    return this;
+  }
 }
