33fea35b113634a96fc4131fa0f4a70e4e42f7e1
wulman16 <will.ulman@gmail.com>
2019-06-06 13:04:17 -0400

    implement post-order DFS method on BST

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -92,4 +92,16 @@ class BinarySearchTree {
     traverse(this.root);
     return visitedNodes;
   }
+
+  DFSPostOrder() {
+    if (!this.root) return [];
+    const visitedNodes = [];
+    const traverse = node => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visitedNodes.push(node.val);
+    };
+    traverse(this.root);
+    return visitedNodes;
+  }
 }
