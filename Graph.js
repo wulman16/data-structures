e9c01afe38ee162fecdfa3362340d7fdb43ff7c0
wulman16 <will.ulman@gmail.com>
2019-06-07 12:07:57 -0400

    implement addVertex and addEdge methods on Graph class

--- Graph.js
+++ Graph.js
@@ -10,8 +10,19 @@
   - Faster to look up a specific edge
 */
 
+// undirected, unweighted graph
+// lacks error handling
 class Graph {
   constructor() {
     this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
