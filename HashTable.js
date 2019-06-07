58b4af11526ae887f189e1b2141b5e4993aebb52
wulman16 <will.ulman@gmail.com>
2019-06-07 10:41:49 -0400

    add improved practice hash function

--- HashTable.js
+++ HashTable.js
@@ -1,6 +1,5 @@
 // example hash function
 // Problems:
-// - only works with strings
 // - takes linear time :(
 // - not very random
 const hash = (key, arrayLen) => {
@@ -11,4 +10,16 @@ const hash = (key, arrayLen) => {
   return total;
 };
 
-console.log(hash(`pink`, 10));
+// another example hash function
+// minimizes collisions with the use of a prime number
+// slight improvement in efficiency
+const betterHash = (key, arrayLen) => {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+};
