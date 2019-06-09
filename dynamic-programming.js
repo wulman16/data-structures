90a34380e83f08c6bd244df2ce46fddb166f7357
wulman16 <will.ulman@gmail.com>
2019-06-09 15:47:02 -0400

    implement fibonacci function using memoization

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -1,5 +1,17 @@
 // calculate the nth number in the fibonacci sequence
+// Time complexity: O(2^n) :'(
 const fibonacci = n => {
   if (n <= 2) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+const dynamicFibonacci = n => {
+  let memo = {};
+  const fib = n => {
+    if (memo[n]) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fib(n - 1) + fib(n - 2);
+    return memo[n];
+  };
+  return fib(n);
 };
