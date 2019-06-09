85f8b0c1bcf77a15706ab25aecee164ca96088bb
wulman16 <will.ulman@gmail.com>
2019-06-09 13:14:02 -0400

    use more efficient priority queue for Dijkstra's algorithm

--- WeightedGraph.js
+++ WeightedGraph.js
@@ -84,23 +84,77 @@ class WeightedGraph {
   }
 }
 
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  // Time complexity: O(n(log(n)))
+  // Time complexity: O(log(n))
   enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this._sort();
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    let idx = this.values.length - 1;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    while (idx > 0) {
+      if (this.values[parentIdx].priority <= newNode.priority) break;
+      [this.values[parentIdx], this.values[idx]] = [
+        this.values[idx],
+        this.values[parentIdx]
+      ];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    return this;
   }
 
+  // Time complexity: O(log(n))
   dequeue() {
-    return this.values.shift();
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
 
-  _sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 }
 
