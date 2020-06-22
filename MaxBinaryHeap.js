72f90982ac1a1f854b410c8408ce79f8181e9e4d
Will Ulman <will.ulman@gmail.com>
2020-06-21 20:30:06 -0400

    refactor MBH extractMax method

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -32,40 +32,40 @@ class MaxBinaryHeap {
     };
 
     if (this.values.length === 0) return null;
-
     swap(this.values, 0, this.values.length - 1);
     const max = this.values.pop();
-
     if (this.values.length === 0) return max;
 
-    let recentIdx = 0;
-    let leftIdx = recentIdx * 2 + 1;
+    let currentIdx = 0;
+    let leftIdx = currentIdx * 2 + 1;
     let rightIdx = leftIdx + 1;
+    let currentVal = this.values[currentIdx];
     let leftVal = this.values[leftIdx];
     let rightVal = this.values[rightIdx];
 
     while (leftVal) {
       if (!rightVal) {
-        if (this.values[recentIdx] > leftVal) break;
+        if (currentVal > leftVal) break;
         else {
-          swap(this.values, recentIdx, leftIdx);
+          swap(this.values, currentIdx, leftIdx);
           break;
         }
       }
-      if (this.values[recentIdx] > leftVal && this.values[recentIdx] > rightVal)
-        break;
+      if (currentVal > leftVal && currentVal > rightVal) break;
       if (leftVal > rightVal) {
-        swap(arr, recentIdx, leftIdx);
-        recentIdx = leftIdx;
+        swap(this.values, currentIdx, leftIdx);
+        currentIdx = leftIdx;
       } else {
-        swap(arr, recentIdx, rightIdx);
-        recentIdx = rightIdx;
+        swap(this.values, currentIdx, rightIdx);
+        currentIdx = rightIdx;
       }
-      leftIdx = recentIdx * 2 + 1;
+      leftIdx = currentIdx * 2 + 1;
       rightIdx = leftIdx + 1;
+      currentVal = this.values[currentIdx];
       leftVal = this.values[leftIdx];
       rightVal = this.values[rightIdx];
     }
+
     return max;
   }
 }
