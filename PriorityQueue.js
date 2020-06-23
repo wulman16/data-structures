d499256d82b49aa95777f47be28272581e31e722
Will Ulman <will.ulman@gmail.com>
2020-06-23 18:36:52 -0400

    refactor priority queue enqueue method

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -12,16 +12,17 @@ class PriorityQueue {
 
   // Time complexity: O(log(n))
   enqueue(val, priority) {
+    const swap = (arr, i, j) => {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+
     const newNode = new Node(val, priority);
     this.values.push(newNode);
     let idx = this.values.length - 1;
     let parentIdx = Math.floor((idx - 1) / 2);
     while (idx > 0) {
       if (this.values[parentIdx].priority <= newNode.priority) break;
-      [this.values[parentIdx], this.values[idx]] = [
-        this.values[idx],
-        this.values[parentIdx],
-      ];
+      swap(this.values, parentIdx, idx);
       idx = parentIdx;
       parentIdx = Math.floor((idx - 1) / 2);
     }
