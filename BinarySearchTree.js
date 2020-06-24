62a6c1bb3dbea9ce4f77f8640b5484069a4c296b
Will Ulman <will.ulman@gmail.com>
2020-06-24 16:00:24 -0400

    refactor BST BFS method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -75,12 +75,10 @@ class BinarySearchTree {
 
   breadthFirstSearch() {
     if (!this.root) return [];
-    const queue = [];
     const result = [];
-    let node = this.root;
-    queue.push(node);
-    while (queue.length) {
-      node = queue.shift();
+    const queue = [this.root];
+    while (queue.length > 0) {
+      let node = queue.shift();
       result.push(node.val);
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
